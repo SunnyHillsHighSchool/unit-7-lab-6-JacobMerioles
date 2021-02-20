4947dbf8348d6c804ab36773fca76662b3650bda
Jacob Merioles <600021653@fjuhsd.org>
2021-02-20 17:26:24 +0000

    Finished Unit 7 Lab 6

--- Word.java
+++ Word.java
@@ -1,21 +1,53 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Jacob Merioles
 
 import static java.lang.System.*;
 
 public class Word
 {
-   private static String vowels = "AEIOUaeiou";   	//why is this here?
-   																//why is it static?
+   private static String vowels = "AEIOUaeiou"; //why is this here?
+   	//why is it static? this is a class variable
    
    //add an instance variable
+  private String s;
 
-	//create a constructor
-	
-	//create other methods
-	//look at the sample runner code
-	//on the lab handout
+	//create 2 constructors
+	public Word(){
+    s = "";
+  }
+  public Word(String str){
+    s = str;
+  }
+
+  //setter method
+  public void setWord(String change){
+    s = change;
+  }
+
+  //getter method
+  public String getWord(){
+    return s;
+  }
+
+  //getNumVowels
+  public int getNumVowels(){
+    int count = 0;
+    for(int i = 0; i < s.length()-1; i++){
+      for(int x = 0; x < vowels.length()-1; x++)
+        if (s.charAt(i) == vowels.charAt(x))
+          count++;
+    }
+    return count;
+  }
+
+  //getLength
+  public int getLength(){
+    return s.length();
+  }
 
 	//create a toString method
+  public String toString(){
+    return ""+s;
+  }
 }
